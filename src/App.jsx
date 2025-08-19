f4d0e4fdb0e021f6c68b569ac56e09f257a171fd
Lenovo T480 <anasrafiqanas@gmail.com>
2025-08-19 14:00:24 +0500

    Clothing

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/home/Hero'
+import Mostpopular from './components/common/Mostpopular'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Hero/>
+    <Mostpopular/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/common/Mostpopular.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Mostpopular = () => {
+  return (
+    <div>
+    <div className='bg-white flex justify-center'>
+      <h1 className=' font-serif italic text-6xl mt-24 '> Most Popular </h1>
+    </div>
+    <div>
+        
+    </div>
+    </div>
+  )
+}
+
+export default Mostpopular
